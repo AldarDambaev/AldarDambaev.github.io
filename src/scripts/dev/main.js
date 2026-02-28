cd745230c70167f7d8f665e850ae17ababa45c53
aldardambaev <repeater.dv@gmail.com>
2026-03-01 01:08:17 +0300

    popup js

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -6,4 +6,38 @@
     navToggle.addEventListener("click", function(){
         root.classList.toggle("show-nav");
     });
+
+    const eventPP = document.querySelector("#js-eventPP");
+
+
+
+    if (eventPP) {
+        const eventOpenBtn = document.querySelector("#eventOpenBtn");
+
+        const closeEventPP = function(event) {
+            function close() {
+            document.removeEventListener("keyup", closeEventPP);
+            eventPP.removeEventListener("click", closeEventPP);
+            root.classList.remove("show-event-popup");
+        };
+
+        switch (event.type) {
+            case "keyup":
+                if (event.key === "Escape" ) close();
+                break;
+                case "click":
+                    if (event.target === this || event.target.classList.contains("js-ppCloseBtn")) {
+                        close();
+                        break;
+                    }
+        }
+    };
+
+        eventOpenBtn.addEventListener("click", function() {
+            document.addEventListener("keyup", closeEventPP);
+            event.addEventListener("click", closeEventPP);
+        });
+
+
+    }
 })();
